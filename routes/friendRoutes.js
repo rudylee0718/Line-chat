4e7846c9a6e978707dc6f0fff4f0f093a0fd86d2
rudylee0718 <klinsmann07187@gmail.com>
2025-08-19 13:43:56 +0800

    Update backend code

--- routes/friendRoutes.js
+++ routes/friendRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { sendFriendRequest, acceptFriendRequest, getFriendRequests } = require('../controllers/friendRequestController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/auth');
 
 // 發送好友請求
 router.post('/request', protect, sendFriendRequest);
